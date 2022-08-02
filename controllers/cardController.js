f52883e492937bfa27636c0b53908e32d08e0aee
000Ren000 <antonren@yandex.ru>
2022-08-02 15:12:45 +0700

    add / Добавил обработку ошибок

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,14 +1,21 @@
 const Card = require('../models/card');
+const sendErrorMessage = (err, res) => {
+	if (err.name === 'CastError')
+		return res.status(400).send({message: 'Запрашиваемая карточка не найдена'});
+	if (err.name === 'ValidationError')
+		return res.status(400).send({message: 'Не правильно введены данные'});
+	return res.status(500).send({message: `Внутренняя ошибка сервера`});
+}
 
 //Методы работы с карточками
 
 // GET /cards — возвращает все карточки
 module.exports.getCardAll = async (req, res) => {
 	try {
-		const  allCard = await Card.find({});
+		const allCard = await Card.find({});
 		await res.send(200, allCard);
 	} catch (err) {
-		res.status(500).json({message: 'Не получилось получить данные карточек'})
+		sendErrorMessage(err, res)
 	}
 }
 
@@ -16,40 +23,46 @@ module.exports.getCardAll = async (req, res) => {
 module.exports.createCard = async (req, res) => {
 	try {
 		const {name, link} = req.body;
-		const newCard = await Card.create({name, link, owner:req.user._id});
+		const newCard = await Card.create({name, link, owner: req.user._id});
 		await res.send(200, newCard);
 	} catch (err) {
-		res.status(500).json({message: 'Не получилось cоздать карточку'})
+		sendErrorMessage(err, res)
 	}
 }
 
 // DELETE /cards/:cardId — удаляет карточку по идентификатору
 module.exports.deleteCard = async (req, res) => {
-try {
-	const {cardId} = req.params;
-	await Card.deleteOne({_id: cardId});
-	await res.send(200, {message: "Карточка удалена!"})
-}catch (err) {res.status(500).json({message:'Не получилось удалить карточку'})}
+	try {
+		const {cardId} = req.params;
+		await Card.deleteOne({_id: cardId});
+		await res.send(200, {message: "Карточка удалена!"})
+	} catch (err) {
+		sendErrorMessage(err, res)
+	}
 }
 
 module.exports.likeCard = async (req, res) => {
 	try {
 		const newCard = await Card.findByIdAndUpdate(
 				req.params.cardId,
-				{ $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-				{ new: true },
+				{$addToSet: {likes: req.user._id}}, // добавить _id в массив, если его там нет
+				{new: true},
 		)
 		await res.send(200, newCard);
-	}catch (err) {res.status(500).json({message:'Не получилось поставить лайк'})}
+	} catch (err) {
+		sendErrorMessage(err, res)
+	}
 }
 
 module.exports.dislikeCard = async (req, res) => {
 	try {
 		const newCard = await Card.findByIdAndUpdate(
 				req.params.cardId,
-				{ $pull: { likes: req.user._id } },
-				{ new: true },
+				{$pull: {likes: req.user._id}},
+				{new: true},
 		);
 		await res.send(200, newCard);
-	}catch (err) {res.status(500).json({message:'Не получилось удалить лайк'})}
+	} catch (err) {
+		sendErrorMessage(err, res)
+	}
 }
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,45 +1,48 @@
 const User = require('../models/user');
+const sendErrorMessage = (err, res) => {
+	if (err.name === 'CastError')
+		return res.status(400).send({message: 'Запрашиваемый пользователь не найден.'});
+	if (err.name === 'ValidationError')
+		return res.status(400).send({message: 'Не правильно введены данные'});
+	return res.status(500).send({message: `Внутренняя ошибка сервера`});
+}
 
 module.exports.getUsersAll = async (req, res) => {
 	try {
 		const users = await User.find({})
 		await res.send(200, users);
-	}
-	catch (err) {res.status(500).json({message:'Не получилось получить данные о пользователях'})}
+	} catch (err) {sendErrorMessage(err, res)}
 }
 
 module.exports.getUser = async (req, res) => {
 	try {
 		const user = await User.find({_id: req.params.userId})
 		await res.send(200, user);
-	}
-	catch (err) {res.status(500).json({message:'Не получилось получить данные о пользователях'})}
+	} catch (err) {sendErrorMessage(err, res)}
+
 }
 
 module.exports.createUser = async (req, res) => {
 	try {
-		const { name, about, avatar } = req.body; // получим из объекта запроса имя и описание пользователя
-		const newUser = await User.create({ name, about, avatar });
+		const {name, about, avatar} = req.body; // получим из объекта запроса имя и описание пользователя
+		const newUser = await User.create({name, about, avatar});
 		await res.send(200, newUser);
-	}
-	catch (err) {res.status(500).json({message:'Не получилось cоздать пользователя'})}
+	} catch (err) {sendErrorMessage(err, res)}
 }
 
 module.exports.updateProfile = async (req, res) => {
 	try {
-		const { name, about } = req.body; // получим из объекта запроса имя и описание пользователя
-		const newUser = await User.findByIdAndUpdate(req.user._id,{ name, about });
+		const {name, about} = req.body; // получим из объекта запроса имя и описание пользователя
+		const newUser = await User.findByIdAndUpdate(req.user._id, {name, about});
 		await res.send(200, newUser);
-	}
-	catch (err) {res.status(500).json({message:'Не получилось cоздать пользователя'})}
+	} catch (err) {sendErrorMessage(err, res)}
 }
 
 module.exports.updateAvatar = async (req, res) => {
 	try {
-		const { avatar} = req.body; // получим из объекта запроса имя и описание пользователя
-		const newUser = await User.findByIdAndUpdate(req.user._id,{ avatar });
+		const {avatar} = req.body; // получим из объекта запроса имя и описание пользователя
+		const newUser = await User.findByIdAndUpdate(req.user._id, {avatar});
 		await res.send(200, newUser);
-	}
-	catch (err) {res.status(500).json({message:'Не получилось cоздать пользователя'})}
+	} catch (err) {sendErrorMessage(err, res)}
 }
 
