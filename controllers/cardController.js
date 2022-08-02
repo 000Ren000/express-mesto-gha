c0ca93a927fea340979ca50db9de6b18b3c211a4
000Ren000 <antonren@yandex.ru>
2022-08-02 19:30:37 +0700

    fix / подправил коды ошибок

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,7 +1,7 @@
 const Card = require('../models/card');
 const sendErrorMessage = (err, res) => {
 	if (err.name === 'CastError')
-		return res.status(400).send({message: 'Запрашиваемая карточка не найдена'});
+		return res.status(404).send({message: 'Запрашиваемая карточка не найдена'});
 	if (err.name === 'ValidationError')
 		return res.status(400).send({message: 'Не правильно введены данные'});
 	return res.status(500).send({message: `Внутренняя ошибка сервера`});
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,7 @@ const sendErrorMessage = (err, res) => {
 	if (err.name === 'CastError')
 		return res.status(400).send({message: 'Запрашиваемый пользователь не найден.'});
 	if (err.name === 'ValidationError')
-		return res.status(400).send({message: 'Не правильно введены данные'});
+		return res.status(404).send({message: 'Не правильно введены данные'});
 	return res.status(500).send({message: `Внутренняя ошибка сервера`});
 }
 
