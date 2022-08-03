1ef3eb06ec965db07373fff9bd43c93c257ae5df
000Ren000 <antonren@yandex.ru>
2022-08-03 15:26:09 +0700

    Забыли указать ref

--- models/card.js
+++ models/card.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const cardSchema = mongoose.Schema({
 	name: { type:String, minLength:2, maxLength:30, require },
 	link: {type:String, require},
-	owner: {type: mongoose.Types.ObjectId, require},
-	likes: [],
+	owner: {ref: 'user', type: mongoose.Types.ObjectId, require},
+	likes: [{type: String, ref: 'user', default: []}],
 	createdAt: {type: Date, default: Date.now()}
 })
 
