13f3e91159daa47212c18c8a8bbebc5c42e721fe
000Ren000 <antonren@yandex.ru>
2022-08-06 17:20:21 +0700

    не установлено значение для require, исправить нужно везд

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, minLength: 2, maxLength: 30, require },
-  about: { type: String, minLength: 2, maxLength: 30, require },
-  avatar: { type: String, require },
+  name: { type: String, minLength: 2, maxLength: 30, require: true },
+  about: { type: String, minLength: 2, maxLength: 30, require: true },
+  avatar: { type: String, require: true },
 });
 
 module.exports = mongoose.model('user', userSchema);
