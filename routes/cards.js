eac6355828e28e0b3af10bf014ff238a4e33a98a
000Ren000 <antonren@yandex.ru>
2022-08-23 15:39:04 +0700

    fix / Id необходимо валидировать как hex последовательность длиной 24 символа

--- routes/cards.js
+++ routes/cards.js
@@ -30,21 +30,21 @@ router.post('/', celebrate({
 // DELETE /cards/:cardId — удаляет карточку по идентификатору
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required().min(2),
+    cardId: Joi.string().length(24).hex().required(),
   }),
 }), deleteCard);
 
 // PUT /cards/:cardId/likes — поставить лайк карточке
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required().min(2),
+    cardId: Joi.string().length(24).hex().required(),
   }),
 }), likeCard);
 
 // DELETE /cards/:cardId/likes — убрать лайк с карточки
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required().min(2),
+    cardId: Joi.string().length(24).hex().required(),
   }),
 }), dislikeCard);
 
