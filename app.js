827aaf01a7d22ab50118ab7e0fbc7c7dc8b67212
000Ren000 <antonren@yandex.ru>
2022-08-25 06:44:08 +0700

    fix / Следует также добавить min - 2 значение для проверки

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().max(30),
-    about: Joi.string().max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string().custom(validateURL),
   }),
 }), createUser);
