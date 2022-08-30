484e29c0a9a7d7ebc9506e406f6663100f2043e9
000Ren000 <antonren@yandex.ru>
2022-08-30 14:24:58 +0700

    fix / cors

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const corsOptions = {
   origin: [
     'https://mesto.antonren.ru',
     'http://mesto.antonren.ru',
-    'http://localhost:3000',
+    'http://localhost:3001'
   ],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
